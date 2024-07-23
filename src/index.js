43e6de4c54b6775284a44245c179a088523f7378
Lavender <lhwang914@gmail.com>
2024-07-22 22:24:08 -0700

    Complete Page Switching Logic

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import loadInitialPage from './loadInitialPage';
 import renderMenuPage from './tabs/menu';
 import renderContactPage from './tabs/contact';
+import './styles.css';
 
 document.addEventListener('DOMContentLoaded', () => {
 	const homeBtn = document.getElementById('home-btn');
 	const menuBtn = document.getElementById('menu-btn');
 	const contactBtn = document.getElementById('contact-btn');
 
+	const clearPage = () => {
+		document.getElementById('content').innerHTML = '';
+	};
+
+	homeBtn.addEventListener('click', () => {
+		clearPage();
+		loadInitialPage();
+	});
+
+	menuBtn.addEventListener('click', () => {
+		clearPage();
+		renderMenuPage();
+	});
+
+	contactBtn.addEventListener('click', () => {
+		clearPage();
+		renderContactPage();
+	});
+
 	loadInitialPage();
-	renderMenuPage();
-	renderContactPage();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
 	module: {
 		rules: [
 			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
 				use: [
 					{
